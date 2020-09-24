4aac2bbb839b95677c211cdcfde9df5f6f5a3887
artsiom.sakratar <artsiom.sakratar@gmail.com>
2020-09-24 05:48:59 +0300

    add Snake game

---
+++ SnakeGameSwiftUI/SnakeGameSwiftUI/SnakeGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameSwiftUIApp.swift
+//  SnakeGameSwiftUI
+//
+//  Created by Artsiom Sakratar on 9/24/20.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
