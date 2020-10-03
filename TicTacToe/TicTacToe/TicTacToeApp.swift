153c3ebaa41045e466f933421be2728fa5a49a6c
artsiom.sakratar <artsiom.sakratar@gmail.com>
2020-10-03 17:24:44 +0300

    add TicTacToe app

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Artsiom Sakratar on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
